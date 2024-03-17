1089a143d2d6d279d782d4461c1b5d8b8d3f22c0
s20336 <80452807+s20336@users.noreply.github.com>
2024-03-17 14:59:22 +0100

    feature-average

--- Initialproject/Program.cs
+++ Initialproject/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, my second World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia: " + average);
+    }
+
+    public static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
